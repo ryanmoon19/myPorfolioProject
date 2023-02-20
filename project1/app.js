6dc1d1a69e8de8f63be9e8c147c13fe04fd61433
ryanmoon19 <ryanhmoon11@gmail.com>
2023-02-20 14:28:17 -0800

    added modal for submit

--- project1/app.js
+++ project1/app.js
@@ -11,4 +11,30 @@ $(() =>{
         })
     })
 
+    window.onscroll = function() {stickyNav()};
+    const navItems = document.getElementsByClassName('hambugerNav');
+    const sticky = navItems.offsetTop;
+
+    function stickyNav(){
+        if(window.pageYOffset >= sticky){
+            navItems.classList.add("sticky")
+        }else{
+            navItems.classList.remove('sticky')
+        }
+    }
+
+    const $openBtn = $('#openModal');
+    const $modal = $('#modal');
+    const $closeBtn = $('#close');
+
+    const openModal = () => {
+        $modal.css('display', 'block');
+    }
+
+    const closeModal = () =>{
+        $modal.css('display', 'none');
+    }
+
+    $openBtn.on('click', openModal);
+    $closeBtn.on('click', closeModal);
 })
